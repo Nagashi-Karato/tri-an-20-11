ac7e85aa8f6bf0eb8a169d5ffa14730c16df6f79
Le Phi Long <nagashikarato1397@gmail.com>
2024-11-15 13:47:44 +0700

    A file was deleted

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import './assets/style/style.css';
+import './assets/style/wickedcss.min.css'
+import './assets/js/javascript.js'
 
+const giftBox = <img id='giftbox' src='./assets/img/giftbox.png' className='giftbox floater' alt='gift box' />
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="bg">
+      <div className="container">
+      <div className="content">
+      <h1>Bạn có quà nè!</h1>
+      <p>Nhấn vào đây để nhận nà &lt;3</p>
+      </div>
+        {giftBox}
+      </div>
+    </main>
   );
 }
 
