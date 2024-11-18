aa9eede6b9104d36910467331cb3e441e25f8890
Nagashi-Karato <74326498+Nagashi-Karato@users.noreply.github.com>
2024-11-19 04:03:43 +0700

    Add files via upload

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,67 +1,69 @@
 const container = document.querySelector('.container');
-        const main = document.querySelector('.bg');
-        const giftbox = document.querySelector('#giftbox');
-        const circleTransition = document.querySelector('#circleTransition');
+const container_2 = document.querySelector('.container-2');
+const main = document.querySelector('.bg');
+const giftbox = document.querySelector('#giftbox');
+const circleTransition = document.querySelector('#circleTransition');
+var audio = document.querySelector('.bgm')
+var logo2 = document.querySelector('.logo2');
+var thiep = document.querySelector('.thiep');
 
-        giftbox.addEventListener('click', () => {
+giftbox.addEventListener('click', giftboxHandle);
 
-            // go Buckeyes!
-            const colors = ["#bb0000", "#ffffff"];
+function giftboxHandle() {
+    giftbox.removeEventListener("click", giftboxHandle);
 
-            // Confetti animation
-            confetti({
-                particleCount: 200,
-                angle: 60,
-                spread: 100,
-                startVelocity: 80,
-                ticks: 250,
-                origin: { x: 0, y: 0.8 },
-                colors: colors,
-            });
+    // go Buckeyes!
+    const colors = ["#bb0000", "#ffffff"];
 
-            confetti({
-                particleCount: 200,
-                angle: 120,
-                ticks: 250,
-                startVelocity: 80,
-                spread: 100,
-                origin: { x: 1, y: 0.8 },
-                colors: colors,
-            });
+    // Confetti animation
+    confetti({
+        particleCount: 200,
+        angle: 60,
+        spread: 100,
+        startVelocity: 80,
+        ticks: 250,
+        origin: { x: 0, y: 0.8 },
+        colors: colors,
+    });
+
+    confetti({
+        particleCount: 200,
+        angle: 120,
+        ticks: 250,
+        startVelocity: 80,
+        spread: 100,
+        origin: { x: 1, y: 0.8 },
+        colors: colors,
+    });
 
 
-            setTimeout(() => {
-                circleTransition.classList.add('circle-expand');
-
-                setTimeout(() => {
-                    container.innerHTML = '';
-                    setTimeout(() => {
-                        transition();
-                    }, 500)
-                }, 500); 
-            }, 2000)
-        });
-        
-        function appear(){
-            const audio = document.createElement('audio');
-            audio.src = './audio/candyland.mp3'
-            audio.autoplay = true
-            document.body.append(audio);
-
-            const img = document.createElement('img');
-            img.src = './img/logo2.png';
-            container.appendChild(img);
-            img.classList = 'logo2 animate__animated animate__fadeInDown animate__delay-1s';
-
-            
-        }
-
-        function transition(){
+    setTimeout(() => {
+        circleTransition.classList.add('circle-expand');
+        setTimeout( () => {
             main.style.backgroundImage = 'url("./img/Nen2.jpg")';
-            circleTransition.classList.remove('animate__fadeIn');            circleTransition.classList.remove('animate__fadeIn');
-            circleTransition.classList.remove('circle-expand');
-            setTimeout(() => {
-                appear();
-            }, 500)
-        }
-        
+            container.getElementsByClassName('container-1')[0].remove();    
+            transition();
+        }, 1500)
+
+    }, 2000)
+}
+
+
+function appear() {
+    container_2.style.display = "block";
+    logo2.classList = 'logo2 animate__animated animate__fadeInDown animate__delay-1s';
+    audio.autoplay = true;
+    setTimeout( () => {
+        thiep.style.display = 'flex';
+        thiep.className += ' animate__animated animate__fadeInDown animate__delay-1s';
+    }, 500)
+}
+
+function transition() {
+    circleTransition.classList.remove('animate__fadeIn');
+    circleTransition.classList.remove('circle-expand');
+    audio.play();
+    setTimeout(() => {
+        appear();
+    }, 500)
+}
